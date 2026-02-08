e364ee6636600a3af568949abebb7aa117eddf23
Pavel Shalianhouski <pshalianhouski@interventioninsights.com>
2026-02-08 23:13:44 +0100

    Implemented flyway migration for creating simple_park_parking table

--- src/main/resources/db/migration/V1__create_simple_park_parking.sql
+++ src/main/resources/db/migration/V1__create_simple_park_parking.sql
@@ -1,2 +1,12 @@
--- TODO: Create the simple_park_parking table
---
+CREATE TABLE simple_park_parking (
+    id SERIAL PRIMARY KEY,
+    internal_parking_id VARCHAR(255) UNIQUE NOT NULL,
+    external_parking_id VARCHAR(255),
+    license_plate VARCHAR(32) NOT NULL,
+    area_code VARCHAR(32) NOT NULL,
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP,
+    status VARCHAR(8) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+);
